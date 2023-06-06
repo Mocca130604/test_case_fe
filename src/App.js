69b0f0be8f86e8dafd7b86e35fcafca70e86f955
Mocca13 <62732903+Mocca130604@users.noreply.github.com>
2023-06-06 18:03:12 +0700

    show hasil

--- src/App.js
+++ src/App.js
@@ -54,6 +54,10 @@ function App() {
   };
 
   const save = () => {
+    const variable = JSON.stringify(AspectScoreArray);
+    localStorage.setItem("hasil", variable);
+    const hasil = localStorage.getItem("hasil");
+    console.log(hasil);
     setIsSave(!isSave);
   };
 
@@ -154,52 +158,15 @@ function App() {
   const Modal = () => {
     return (
       <div
-        id="defaultModal"
+        style={{ display: isSave ? "block" : "none" }}
         tabindex="-1"
         class="flex align-center justify-center w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
       >
-        <div class="relative w-full max-w-2xl max-h-full">
-          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-              <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                Terms of Service
-              </h3>
-              <button
-                type="button"
-                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                data-modal-hide="defaultModal"
-              >
-                <svg
-                  aria-hidden="true"
-                  class="w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span class="sr-only">Close modal</span>
-              </button>
-            </div>
-            <div class="p-6 space-y-6">AspectScoreArray.aspek_penilaian_1</div>
-            <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-              <button
-                data-modal-hide="defaultModal"
-                type="button"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                I accept
-              </button>
-            </div>
-          </div>
-        </div>
+        <div class="p-6 space-y-6 ">{localStorage.getItem("hasil")}</div>
       </div>
     );
   };
+
   return (
     <div className="App">
       <div className="container p-10">
